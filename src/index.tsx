b7427f6355e74f903a054c394b55e0b9f27cded7
Evgeny <80065479+evgeny224@users.noreply.github.com>
2021-12-11 11:00:33 +0300

    add eslint changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// // const eleemnt = <h1>Hello World!</h1>;
+// const lementTwo = React.createElement(
+//   "h1",
+//   { className: "test" },
+//   "Привет Мир!"
+// );
+// ReactDOM.render(lementTwo, document.getElementById("root"));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

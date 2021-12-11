93c4c626031ecc350560c38df1284504e762d70d
Evgeny <80065479+evgeny224@users.noreply.github.com>
2021-12-11 13:10:40 +0300

    after lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import ProductCounter from "./ProductCounter";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <ProductCounter product={{ productName: "Апельсин", count: 6 }} />
+      <ProductCounter product={{ productName: "Яблоко" }} />
+      <ProductCounter product={{ productName: "Ананас", count: 6 }} />
+      <ProductCounter product={{ productName: "Арбуз", count: 0 }} />
+      <ProductCounter product={{ productName: "Дыня" }} />
     </div>
   );
 };
